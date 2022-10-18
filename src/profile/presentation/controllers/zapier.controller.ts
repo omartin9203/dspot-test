c66bd2ccdd3adab7e6e9d3b688d7819b4a7c1852
Oscar Martin Glez-Chavez <o.martin9203@gmail.com>
2022-10-18 09:52:50 -0400

    feat(zapier): adding endpoint to candidate_events

--- src/profile/presentation/controllers/zapier.controller.ts
+++ src/profile/presentation/controllers/zapier.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Headers, Res } from '@nestjs/common';
+import { Controller, Get, Headers, Query, Res } from '@nestjs/common';
 import { BaseController } from '../../../shared/core/presentation/BaseController';
 import { Response } from 'express';
 import { AppConfigService } from '../../../shared/modules/config/service/app-config-service';
+import { PaginatedFindProfileQuery } from '../../aplication/queries/impl/paginated-find-profile.query';
+import { QueryBus } from '@nestjs/cqrs';
+import { PaginatedFindProfileUseCaseResp } from '../../aplication/use-cases/paginated-find-profile/paginated-find-profile.use-case';
+import { ProfileMapper } from '../../infrastructure/mapper/profile.mapper';
+
+type CandidateEventsQuery = {
+  api_key?: string;
+  candidate_hash?: string;
+  job_hash?: string;
+  page?: string;
+  limit?: string;
+};
 
 @Controller('zapier')
 export class ZapierController extends BaseController {
-  constructor(readonly config: AppConfigService) {
+  constructor(readonly config: AppConfigService, readonly _qBus: QueryBus) {
     super();
   }
   @Get('me')
@@ -15,5 +27,29 @@ export class ZapierController extends BaseController {
       name: 'test',
       email: 'test@test.com',
     });
+  }
+
+  @Get('candidate_events')
+  async candidateEvents(
+    @Res() response: Response,
+    @Query() query: CandidateEventsQuery,
+    @Headers('X-API-KEY') apiKey?: string
+  ): Promise<Response> {
+    if (!apiKey || apiKey != this.config.app.jwtSecret) return this.unauthorized(response);
+    const resp: PaginatedFindProfileUseCaseResp = await this._qBus.execute(
+      new PaginatedFindProfileQuery({
+        pageParams: {
+          pageNum: !isNaN(Number(query.page)) ? +query.page + 1 : 1,
+          pageLimit: !isNaN(Number(query.limit)) ? +query.limit : 10,
+        },
+        includes: ['friends'],
+        order: {
+          createdAt: 'DESC',
+        },
+      })
+    );
+    if (resp.isFailure) return this.fail(response, resp.unwrapError());
+    const paginated = resp.unwrap();
+    return this.ok(response, paginated.items.map(ProfileMapper.DomainToDto));
   }
 }
